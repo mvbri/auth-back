1a67d409ca47b410d7502430143224cd6fcb2011
Maria Briceño <mvbri10@gmail.com>
2025-02-21 19:41:55 -0400

    Feat: implementa conexión con la base de datos

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 
-require("dotenv").config;
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+async function main() {
+  await mongoose.connect(process.env.DB_CONNECTION_STRING);
+  console.log("Connected to MongoBD");
+}
+
+main().catch(console.error);
 
 app.use("/api/signup", require("./routes/signup"));
 app.use("/api/user", require("./routes/user"));
