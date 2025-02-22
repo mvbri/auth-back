9941c35e3f73afaad32c5521c6fc9c8156aaa4e7
Maria Briceño <mvbri10@gmail.com>
2025-02-21 20:57:07 -0400

    Feat: Hashea password de usuario

--- schema/user.js
+++ schema/user.js
@@ -1,8 +1,26 @@
 const { Mongoose } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Mongoose.Schema({
   id: { type: Object },
   username: { type: String, require: true, unique: true },
   password: { type: String, require: true },
   name: { type: String, require: true },
+});
+
+UserSchema.pre("save", function (next) {
+  if (this.isModified("password") || this.isNew) {
+    const document = this;
+
+    bcrypt.hash(document.password, 10, (err, hash) => {
+      if (err) {
+        next(err);
+      } else {
+        document.password = hash;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
 });
